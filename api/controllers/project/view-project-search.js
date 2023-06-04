160ec937662451523882b4f69689466cda7f0f10
Rogerio Fersan <rogerio.fersan.dev@gmail.com>
2023-06-04 17:54:10 -0300

    SNAPSHOT

---
+++ api/controllers/project/view-project-search.js
@@ -0,0 +1,27 @@
+module.exports = {
+
+
+  friendlyName: 'View project search',
+
+
+  description: 'Display "Project search" page.',
+
+
+  exits: {
+
+    success: {
+      viewTemplatePath: 'pages/project/project-search'
+    }
+
+  },
+
+
+  fn: async function () {
+
+    // Respond with view.
+    return {};
+
+  }
+
+
+};
---
+++ api/models/Project.js
@@ -0,0 +1,39 @@
+/**
+ * Project.js
+ *
+ * @description :: A model definition represents a database table/collection.
+ * @docs        :: https://sailsjs.com/docs/concepts/models-and-orm/models
+ */
+
+module.exports = {
+
+  attributes: {
+
+    //  ╔═╗╦═╗╦╔╦╗╦╔╦╗╦╦  ╦╔═╗╔═╗
+    //  ╠═╝╠╦╝║║║║║ ║ ║╚╗╔╝║╣ ╚═╗
+    //  ╩  ╩╚═╩╩ ╩╩ ╩ ╩ ╚╝ ╚═╝╚═╝
+
+    taskName: {type: 'string', required: true},
+    taskDescription: {type: 'string', required: true},
+    complexity: {type: 'number', required: true},
+
+    todo: {type: 'ref',columnType: 'boolean'},
+    doing: {type: 'ref',columnType: 'boolean'},
+    done: {type: 'ref',columnType: 'boolean'},
+
+    //  ╔═╗╔╦╗╔╗ ╔═╗╔╦╗╔═╗
+    //  ║╣ ║║║╠╩╗║╣  ║║╚═╗
+    //  ╚═╝╩ ╩╚═╝╚═╝═╩╝╚═╝
+
+
+    //  ╔═╗╔═╗╔═╗╔═╗╔═╗╦╔═╗╔╦╗╦╔═╗╔╗╔╔═╗
+    //  ╠═╣╚═╗╚═╗║ ║║  ║╠═╣ ║ ║║ ║║║║╚═╗
+    //  ╩ ╩╚═╝╚═╝╚═╝╚═╝╩╩ ╩ ╩ ╩╚═╝╝╚╝╚═╝
+    userTodo: {type: 'string', required: false},
+    userDoing: {type: 'string', required: false},
+    userDone: {type: 'string', required: false},
+
+  },
+
+};
+
