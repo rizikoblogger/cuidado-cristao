91c7b4f4fb4f7354ae76ef20f3c10e844072c5a9
vinimartinsc <vinimartinsc@gmail.com>
2023-12-31 10:12:05 -0300

    bug: correct resource path in menu

--- api/services/menuService.js
+++ api/services/menuService.js
@@ -17,7 +17,7 @@ module.exports = {
       services.push(
         { 'title': sails.__('Churches'), 'href': '/church/search-church', 'description': sails.__('Manage religious organizations') },
         { 'title': sails.__('Groups'), 'href': '/classe/search-classe', 'description': sails.__(`Manage groups in your organization`) },
-        { 'title': sails.__('Resources'), 'href': '/resource/search-classe', 'description': sails.__(`Manage Resources in a organization`) },
+        { 'title': sails.__('Resources'), 'href': '/resource/search-resource', 'description': sails.__(`Manage Resources in a organization`) },
         { 'title': sails.__('Sermons'), 'href': '/sermon/search-sermon', 'description': sails.__(`Manage Sermons of your church`) },
         { 'title': sails.__('Users'), 'href': '/user/search-user', 'description': sails.__(`Manage Users of your church`) },
         { 'title': sails.__('Social Service'), 'href': '/social/social-search', 'description': sails.__(`Manage Sermons of your church`) },
