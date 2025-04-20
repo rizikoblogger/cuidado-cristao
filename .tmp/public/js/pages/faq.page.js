fad5f7ec6e6a87a59ab4fcfcf16855cc6810c117
Rogerio F <rogerio.fersan.dev@gmail.com>
2025-04-20 13:39:02 -0300

    Fix typo in package.json description
    Corrected a misspelling of "community" in the project description to improve clarity and professionalism. No functionality or logic was affected.

---
+++ .tmp/public/js/pages/faq.page.js
@@ -0,0 +1,28 @@
+parasails.registerPage('faq', {
+  //  ╦╔╗╔╦╔╦╗╦╔═╗╦    ╔═╗╔╦╗╔═╗╔╦╗╔═╗
+  //  ║║║║║ ║ ║╠═╣║    ╚═╗ ║ ╠═╣ ║ ║╣
+  //  ╩╝╚╝╩ ╩ ╩╩ ╩╩═╝  ╚═╝ ╩ ╩ ╩ ╩ ╚═╝
+  data: {
+    listEnglish: [],
+    listPortuguese: []
+  },
+
+  //  ╦  ╦╔═╗╔═╗╔═╗╦ ╦╔═╗╦  ╔═╗
+  //  ║  ║╠╣ ║╣ ║  ╚╦╝║  ║  ║╣
+  //  ╩═╝╩╚  ╚═╝╚═╝ ╩ ╚═╝╩═╝╚═╝
+  beforeMount: function() {
+    //…
+  },
+  mounted: async function(){
+    _.extend(this, window.SAILS_LOCALS)
+    this.listEnglish = this.list.filter(faq=>faq.lang===`en`)
+    this.listPortuguese = this.list.filter(faq=>faq.lang===`pt`)
+  },
+
+  //  ╦╔╗╔╔╦╗╔═╗╦═╗╔═╗╔═╗╔╦╗╦╔═╗╔╗╔╔═╗
+  //  ║║║║ ║ ║╣ ╠╦╝╠═╣║   ║ ║║ ║║║║╚═╗
+  //  ╩╝╚╝ ╩ ╚═╝╩╚═╩ ╩╚═╝ ╩ ╩╚═╝╝╚╝╚═╝
+  methods: {
+    //…
+  }
+})
